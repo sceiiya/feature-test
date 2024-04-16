f4d4b8573c7c9ef69a9b8a79baaeba090c202458
sceiiya <meetscheidj@gmail.com>
2024-04-16 23:58:21 +0800

    add pdf route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PDFController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,5 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Route::get('/generate-pdf', 'PDFController@generatePDF');
+Route::get('/generate-pdf', [PDFController::class, 'generatePDF']);
 
 require __DIR__.'/auth.php';
