593ee38922d01c1fdcd5d5311d76fb5dc31bb8a7
sceiiya <meetscheidj@gmail.com>
2024-04-19 23:41:05 +0800

    update controller

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,28 +3,75 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\PdfWatermarker\Watermark\Pdf;
-use Spatie\PdfWatermarker\Watermarker;
 use Illuminate\Support\Facades\Storage;
+
+use FilippoToso\PdfWatermarker\Support\Pdf;
+use FilippoToso\PdfWatermarker\PdfWatermarker;
+use FilippoToso\PdfWatermarker\Support\Position;
+use FilippoToso\PdfWatermarker\Watermarks\ImageWatermark;
+use FilippoToso\PdfWatermarker\Facades\TextWatermarker;
 
 class PDFController extends Controller
 {
-    // public function generatePDF()
-    // {
-    //     // Load view data
-    //     $data = [
-    //         'content' => 'Hello, this is a PDF document.'
-    //     ];
 
-    //     // Generate PDF
-    //     $pdf = PDF::loadView('pdf.stamp', $data);
+    public function ping(Request $req)
+    {
+        return response($req);
+    }
 
-    //     // Add stamp
-    //     $pdf->getDomPDF()->getCanvas()->page_text(50, 50, 'Confidential', null, 24, array(255,0,0));
+    public function stamp(Request $request)
+    {
 
-    //     // Output PDF
-    //     return $pdf->stream('example.pdf');
-    // }
+
+        return TextWatermarker::input('input.pdf')
+            ->output('laravel-text.pdf')
+            ->position(Position::BOTTOM_CENTER, -50, -10)
+            ->asBackground()
+            ->pageRange(3, 4)
+            ->text('Copyrighted')
+            ->angle(25)
+            ->font('arial.ttf')
+            ->size('25')
+            ->color('#CC00007F')
+            ->resolution(300) // 300 dpi
+            ->save()
+            ->download();
+
+    }
+    
+    public function uploadView (Request $request)
+    {
+        return view('pdf.stamp');
+    }
+    
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:pdf|max:2048',
+        ]);
+
+        $file = $request->file('file');
+        $fileName = $file->getClientOriginalName();
+        $filePath = $file->storeAs('pdfs', $fileName);
+
+        return response()->json(['filePath' => $filePath]);
+    }
+
+    public function retrieve(Request $request)
+    {
+        $filePath = $request->input('filePath');
+    
+        // Retrieve the contents of the PDF file
+        $pdfContents = Storage::get($filePath);
+    
+        // Now you can do whatever you want with the PDF contents
+        // For example, you can return the contents as a response to download the file
+        return response($pdfContents, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="example.pdf"'
+        ]);
+    }
+    
     public function generatePDF(Request $request)
     {
         // Generate PDF
@@ -48,10 +95,12 @@ class PDFController extends Controller
 
         return response()->download($filePath);
     }
-        public function upload()
-    {
-        return view('upload');
-    }
+
+
+    //     public function upload()
+    // {
+    //     return view('upload');
+    // }
 
     public function store(Request $request)
     {
